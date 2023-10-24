c9c348edba299409715c30c57721ec501353a8f9
Vitória <vivisoares241005@gmail.com>
2023-10-24 14:02:45 -0300

    roteiro1

--- Roteiro1/src/CalculadoraInvestimentos.java
+++ Roteiro1/src/CalculadoraInvestimentos.java
@@ -1,21 +1,21 @@
+package src;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
 
 public class CalculadoraInvestimentos extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textDepositomensal;
+	private JTextField textDepositoMensal;
 	private JTextField textNumdeMeses;
+	private JTextField textJurosaoMes;
 
 	/**
 	 * Launch the application.
@@ -45,45 +45,40 @@ public class CalculadoraInvestimentos extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(37, 132, 169, 19);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JLabel lblDepositoMensal = new JLabel("Depósito mensal R$:");
+		lblDepositoMensal.setBounds(55, 10, 126, 13);
+		contentPane.add(lblDepositoMensal);
 		
-		textDepositomensal = new JTextField();
-		textDepositomensal.setBounds(37, 33, 169, 19);
-		contentPane.add(textDepositomensal);
-		textDepositomensal.setColumns(10);
+		textDepositoMensal = new JTextField();
+		textDepositoMensal.setBounds(41, 33, 140, 19);
+		contentPane.add(textDepositoMensal);
+		textDepositoMensal.setColumns(10);
+		
+		JLabel lblNumerodeMeses = new JLabel("Num. de meses:");
+		lblNumerodeMeses.setBounds(55, 62, 104, 13);
+		contentPane.add(lblNumerodeMeses);
 		
 		textNumdeMeses = new JTextField();
-		textNumdeMeses.setBounds(37, 79, 169, 19);
+		textNumdeMeses.setBounds(41, 85, 140, 19);
 		contentPane.add(textNumdeMeses);
 		textNumdeMeses.setColumns(10);
 		
+		JLabel lblJurosaoMes = new JLabel("Juros ao mês %:  ");
+		lblJurosaoMes.setBounds(55, 114, 85, 13);
+		contentPane.add(lblJurosaoMes);
+		
+		textJurosaoMes = new JTextField();
+		textJurosaoMes.setBounds(41, 137, 140, 19);
+		contentPane.add(textJurosaoMes);
+		textJurosaoMes.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Total investidos + juros R$:");
+		lblNewLabel_3.setBounds(41, 166, 140, 13);
+		contentPane.add(lblNewLabel_3);
+		
 		JButton btnCalcular = new JButton("Calcular");
-		btnCalcular.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnCalcular.setBounds(78, 182, 85, 21);
+		btnCalcular.setBounds(74, 189, 85, 21);
 		contentPane.add(btnCalcular);
-		
-		JLabel lblDepositoMensal = new JLabel("Depósito mensal R$:");
-		lblDepositoMensal.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblDepositoMensal.setBounds(66, 10, 140, 13);
-		contentPane.add(lblDepositoMensal);
-		
-		JLabel lblNumdeMeses = new JLabel("Num. de meses:");
-		lblNumdeMeses.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNumdeMeses.setBounds(66, 56, 117, 13);
-		contentPane.add(lblNumdeMeses);
-		
-		JLabel lblJurosdoMes = new JLabel("Juros ao mês %: ");
-		lblJurosdoMes.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblJurosdoMes.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblJurosdoMes.setBounds(66, 108, 106, 13);
-		contentPane.add(lblJurosdoMes);
-		
-		JLabel lblNewLabel = new JLabel("Total investido + juros R$:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel.setBounds(47, 161, 159, 13);
-		contentPane.add(lblNewLabel);
 	}
+
 }
