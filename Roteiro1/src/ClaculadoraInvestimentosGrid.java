2809e7ca5e39167616072d053d34399336ff04b1
Vitória <vivisoares241005@gmail.com>
2023-10-25 08:54:59 -0300

    exercicio dois

--- Roteiro1/src/ClaculadoraInvestimentosGrid.java
+++ Roteiro1/src/ClaculadoraInvestimentosGrid.java
@@ -9,11 +9,16 @@ import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ClaculadoraInvestimentosGrid extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,24 +41,43 @@ public class ClaculadoraInvestimentosGrid extends JFrame {
 	 */
 	public ClaculadoraInvestimentosGrid() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 454, 231);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		contentPane.setLayout(new GridLayout(5, 2, 0, 0));
 		
 		JLabel lblDepositoMensal = new JLabel("Depósito mensal R$:");
 		contentPane.add(lblDepositoMensal);
 		
+		textField = new JTextField();
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
 		JLabel lblNumdeMeses = new JLabel("Num. de meses");
 		contentPane.add(lblNumdeMeses);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
 		
 		JLabel lblJurosaoMes = new JLabel("Juros ao mês %:");
 		contentPane.add(lblJurosaoMes);
 		
+		textField_2 = new JTextField();
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
 		JLabel lblTotalInvestidos = new JLabel("Total investido + juros R$: ");
 		contentPane.add(lblTotalInvestidos);
+		
+		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnCalcular);
 	}
 
 }
